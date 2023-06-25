c5839c88db478a5acc07572d7b89b2e1bfe02cca
Tamjid Ahmed <98009616+tamjidahmed0@users.noreply.github.com>
2023-06-25 21:50:48 +0600

    delete

--- client/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Signup from "./Signup"
-import Register from "./Register"
-import Otp from "./Otp"
-import Dashboard from "./Dashboard"
-import Sidebar from './Sidebar'
-import Team from "./Team"
-
-
-export {Signup, Dashboard, Register, Otp, Sidebar, Team}
